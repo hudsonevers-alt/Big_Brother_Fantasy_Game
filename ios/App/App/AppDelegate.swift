6a44f78c96a11753a242af37ca42041a341a681a
Hudson <hudsonevers@gmail.com>
2026-01-07 16:49:15 -0800

    Fix AppDelegate for Capacitor iOS

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -3,8 +3,10 @@ import Capacitor
 import FirebaseCore
 
 @UIApplicationMain
-class AppDelegate: CAPAppDelegate {
-    override func application(
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
@@ -13,7 +15,26 @@ class AppDelegate: CAPAppDelegate {
         }
 
         print("Firebase configured:", FirebaseApp.app() != nil)
+        return true
+    }
 
-        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    func application(
+        _ app: UIApplication,
+        open url: URL,
+        options: [UIApplication.OpenURLOptionsKey: Any] = [:]
+    ) -> Bool {
+        return ApplicationDelegateProxy.shared.application(app, open: url, options: options)
+    }
+
+    func application(
+        _ application: UIApplication,
+        continue userActivity: NSUserActivity,
+        restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void
+    ) -> Bool {
+        return ApplicationDelegateProxy.shared.application(
+            application,
+            continue: userActivity,
+            restorationHandler: restorationHandler
+        )
     }
 }
